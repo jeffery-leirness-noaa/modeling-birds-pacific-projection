7b98479c68700f675430a987ca63ea1e0d674dfe
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-01-19 22:27:23 +0000

    Add interactive map

--- scripts/_temp.R
+++ scripts/_temp.R
@@ -1,5 +1,3 @@
-library(sf)
-
 # load study area polygon
 sa <- "data/study-area.gpkg" |>
   sf::read_sf()
@@ -24,37 +22,19 @@ ggplot2::ggplot() +
   ggplot2::coord_sf(xlim = c(bbox$xmin, bbox$xmax), ylim = c(bbox$ymin, bbox$ymax)) +
   ggplot2::facet_wrap(~ year)
 
+# summary of total area surveyed each year
+dat |>
+  dplyr::group_by(year) |>
+  dplyr::summarise(total_area = sum(seg_length_km * seg_width_km_lg)) |>
+  print(n = Inf)
 
+# mapview interactive plot
+m <- dat |>
+  # dplyr::filter(year < 1982) |>
+  mapview::mapview(zcol = "year", burst = TRUE, cex = 3, alpha = 0, label = "survey_id", hide = TRUE)
 
-# base map
-m <- ggplot() +
-  theme(legend.position = "bottom",
-        panel.background = element_rect(fill = "gray20"),
-        panel.grid = element_line(color = "gray10", size = 0.3)) +
-  labs(x = NULL,
-       y = NULL,
-       title = spcodes$common_name[spcodes$spp_cd == sp])
-gdc <- guide_colorbar(title.position = "top",
-                      title.hjust = 0.5,
-                      barwidth = unit(70 / length(labels), units = "mm"),
-                      barheight = unit(2, units = "mm"))
-odm <- m +
-  geom_raster(data = r.df[r.df$month %in% month.name[i], ],
-              aes(x = x, y = y, fill = observed)) +
-  geom_sf(data = land.pcs, color = "gray10", fill = "gray10", size = 0.3) +
-  geom_sf(data = states.pcs, color = "gray20", fill = "gray20", size = 0.3) +
-  geom_sf(data = canus.pcs, color = "gray20", fill = "gray20", size = 0.3) +
-  geom_sf(data = lakes.pcs, color = "gray20", fill = "gray20", size = 0.3) +
-  coord_sf(xlim = c(ext(r)[1], ext(r)[2]), ylim = c(ext(r)[3], ext(r)[4])) +
-  facet_wrap(~ month) +
-  scale_fill_viridis(name = NULL,
-                     # name = "Observed density",
-                     option = "turbo",
-                     na.value = "transparent",
-                     limits = c(0, max(r.df[r.df$month %in% month.name[i], "observed"])),
-                     guide = gdc)
-ggsave(here(dir.out, fnm), plot = odm, width = fwidth, height = fheight, units = "in")
-
+mapview::mapshot2(m, url = "data/map-data-by-year.html")
+browseURL("data/map-data-by-year.html")
 
 
 # load NPPSD data
@@ -69,6 +49,9 @@ ck <- sf::st_intersects(sa, nppsd)[[1]]
 nppsd <- nppsd |>
   dplyr::slice(ck)
 janitor::tabyl(nppsd, "survey_id")
+
+
+library(sf)
 
 sa |>
   sf::st_geometry() |>
