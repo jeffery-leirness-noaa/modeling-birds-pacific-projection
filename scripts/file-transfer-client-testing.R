47d753797f97ac53c036224cded38b6b9066fa25
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-03-27 17:57:12 +0000

    Add file-transfer-client-testing.R file

---
+++ scripts/file-transfer-client-testing.R
@@ -0,0 +1,21 @@
+
+# needed to install azure cli to docker image for this to work
+# cannot get `az login` to work; instead must use `az login --use-device-code`
+
+reticulate::source_python("../File-Transfer-Solution/file_transfer.py")
+
+account_url <- "https://nccospacificsbdatastor.blob.core.windows.net"
+container_name <- "processing"
+local_folder <- "data"
+cloud_folder <- ""
+
+ftc <- FileTransferClient(account_url,
+                          container_name = container_name,
+                          local_folder = local_folder,
+                          cloud_folder = cloud_folder)
+
+# transfer files from blob to compute
+ftc$transfer_from_blob_to_compute()
+
+# transfer files from compute to blob
+ftc$upload_folder_to_blob(source_folder = local_folder)
