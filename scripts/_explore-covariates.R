8a961d2b9ab9ba5be630fd3b1a90afed04ca3d21
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-01 19:33:23 +0000

    Add generic function to parse NetCDF file into year-month raster layers

--- scripts/_explore-covariates.R
+++ scripts/_explore-covariates.R
@@ -1,58 +1,91 @@
-unzip("wcra31_daily_1980-2010.zip", exdir = "wcra31_daily_1980-2010")
-unzip("WCNRT.zip", exdir = "WCNRT")
+# unzip("wcra31_daily_1980-2010.zip", exdir = "wcra31_daily_1980-2010")
+# unzip("WCNRT.zip", exdir = "WCNRT")
 
-fn <- "wcra31_daily_1980-2010/wcra31_sst_daily_1980_2010.nc"
+fl <- "data/wcra31_sst_daily_1980_2010.nc"
 
 # this seems to work
-r <- raster::brick(fn)
+r <- raster::brick(fl)
 raster::plot(r[[1]])
 
-r <- ncdf4::nc_open(fn)
+r <- ncdf4::nc_open(fl)
 r <- ncdf4::ncvar_get(r, "sst")
 
 
 # perhaps the most promising
-r <- stars::read_ncdf(fn)
-idx <- time(r) |> 
+r <- stars::read_ncdf(fl)
+idx <- time(r) |>
   lubridate::date() == "1999-01-25"
-r[, , , which(idx)] |> 
+r[, , , which(idx)] |>
   plot()
 
-intervals <- tidyr::expand_grid(year = 1980:2024, month = 1:12) |> 
-  dplyr::mutate(start = lubridate::as_date(paste(year, month, "01", sep = "-")), 
-                end = lubridate::rollforward(start)) |> 
+intervals <- tidyr::expand_grid(year = 1980:2024, month = 1:12) |>
+  dplyr::mutate(start = lubridate::as_date(paste(year, month, "01", sep = "-")),
+                end = lubridate::rollforward(start)) |>
   dplyr::select(start, end)
 
-
-r <- stars::read_ncdf(fn)
-idx <- time(r) |> 
-  lubridate::date() %within% lubridate::interval("1999-01-01", "1999-01-31")
-r[, , , which(idx)] |> 
-  stars::st_as_stars() |> 
-  terra::rast() |> 
-  terra::mean() |> 
+library(lubridate)
+i <- 1
+r <- stars::read_ncdf(fl)
+idx <- time(r) |>
+  lubridate::date() %within% lubridate::interval(intervals$start[i], intervals$end[i])
+r[, , , which(idx)] |>
+  stars::st_as_stars() |>
+  terra::rast() |>
+  terra::mean() |>
   terra::plot()
-d <- r[, , , which(idx)] |> 
+d <- r[, , , which(idx)] |>
   stars::st_as_stars()
-ggplot2::ggplot() + 
-  stars::geom_stars(data = d) + 
-  ggplot2::coord_equal() + 
-  ggplot2::facet_wrap(~time) + 
-  ggplot2::theme_void() + 
-  viridis::scale_fill_viridis() + 
-  ggplot2::scale_x_discrete(expand = c(0, 0)) + 
+ggplot2::ggplot() +
+  stars::geom_stars(data = d) +
+  ggplot2::coord_equal() +
+  ggplot2::facet_wrap(~time) +
+  ggplot2::theme_void() +
+  viridis::scale_fill_viridis() +
+  ggplot2::scale_x_discrete(expand = c(0, 0)) +
   ggplot2::scale_y_discrete(expand = c(0, 0))
 
 
-r <- stars::read_ncdf(fn, var = "sst", ncsub = cbind(start = c(1, 1, 1), count = c(NA, NA, 2)))
-terra::rast(r) |> 
-  terra::plot()
+# r <- stars::read_ncdf(fl, var = "sst", ncsub = cbind(start = c(1, 1, 1), count = c(NA, NA, 2)))
+# terra::rast(r) |>
+#   terra::plot()
 
+
+# create month-year summary rasters
+foo <- function(x, start, end, label) {
+  if (match("time", names(stars::st_dimensions(x))) == 3) {
+    library(lubridate)
+    idx <- time(x) |>
+      lubridate::date() %within% lubridate::interval(start, end) |>
+      which()
+    x <- x[, , , idx] |>  # dplyr::slice(time, idx) can be used if x is not a stars proxy object
+      stars::st_as_stars() |>
+      terra::rast() |>
+      terra::mean()
+    names(x) <- label
+    x
+  }
+}
+r <- stars::read_ncdf(fl)
+intervals <- tibble::tibble(start = time(r) |>
+                              lubridate::as_date() |>
+                              lubridate::rollbackward(roll_to_first = TRUE) |>
+                              unique(),
+                            end = lubridate::rollforward(start),
+                            label = format(start, "%Y-%m")) |>
+  dplyr::rowwise() |>
+  dplyr::group_split()
+
+d <- purrr::map(intervals[1:5], \(x) foo(r, start = x$start, end = x$end, label = x$label)) |>
+  terra::rast()
+
+terra::plot(d)
+
+purrr::walk(d, \(x) terra::writeRaster(x, filename = file.path("output", paste0("wcra31-sst-monthly-", names(x), ".tif"))))
 
 
 
 # this fails
-r <- terra::rast(fn)
+r <- terra::rast(fl)
 
 
 
