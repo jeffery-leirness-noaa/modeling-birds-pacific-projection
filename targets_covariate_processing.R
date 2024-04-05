1e4ef4fe13fc24951732e653e26f48744ddc9e01
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-05 15:24:39 -0500

    Add static branching of covariate processing targets via {tarchetypes} package

--- targets_covariate_processing.R
+++ targets_covariate_processing.R
@@ -3,25 +3,50 @@ library(targets)
 
 # set target options
 tar_option_set(
-  format = "qs"
+  format = "qs",
+  controller = crew::crew_controller_local(workers = 10, seconds_idle = 30)
 )
 
 # source necessary R scripts
 tar_source("R/_functions.R")
 
-list(
-  tar_target(wcra31_sst_daily_file, command = "data/wcra31_sst_daily_1980_2010.nc", format = "file"),
-  tar_target(wcra31_sst_daily, command = stars::read_ncdf(wcra31_sst_daily_file)),
-  tar_target(int_monthly, command = create_intervals_monthly(wcra31_sst_daily)),
-  # tar_target(int_test, command = head(int_monthly)),
-  tar_target(covariates_wcra31_sst,
-             command = create_covariate_output(wcra31_sst_daily,
+values <- tibble::tibble(
+  data_source = file.path("data", c("wcra31_bbv_200_daily_1980_2010.nc", "wcra31_sst_daily_1980_2010.nc")),
+  output_fname = fs::path_file(data_source) |>
+    fs::path_ext_remove() |>
+    stringr::str_split(pattern = "_") |>
+    purrr::map_vec(\(x) paste(x[1:2], collapse = "-"))
+)
+targets <- tarchetypes::tar_map(
+  values = values,
+  names = output_fname,
+  tar_target(file, command = data_source, format = "file", deployment = "main"),
+  tar_target(int_monthly, command = head(create_intervals_monthly(file)), deployment = "main"),
+  tar_target(covariates,
+             command = create_covariate_output(file,
                                                start = int_monthly$start,
                                                end = int_monthly$end,
-                                               fname = "wcra31-sst-monthly",
+                                               fname = paste0(output_fname, "-monthly"),
                                                label = int_monthly$label),
-             pattern = map(intervals),
+             pattern = map(int_monthly),
              format = "file",
              iteration = "vector"
   )
 )
+list(targets)
+
+# list(
+#   # wcra31_sst_daily
+#   tar_target(wcra31_sst_daily_file, command = "data/wcra31_sst_daily_1980_2010.nc", format = "file", deployment = "main"),
+#   tar_target(wcra31_sst_int_monthly, command = head(create_intervals_monthly(wcra31_sst_daily_file)), deployment = "main"),
+#   tar_target(covariates_wcra31_sst,
+#              command = create_covariate_output(wcra31_sst_daily_file,
+#                                                start = wcra31_sst_int_monthly$start,
+#                                                end = wcra31_sst_int_monthly$end,
+#                                                fname = "wcra31-sst-monthly",
+#                                                label = wcra31_sst_int_monthly$label),
+#              pattern = map(wcra31_sst_int_monthly),
+#              format = "file",
+#              iteration = "vector"
+#   )
+# )
