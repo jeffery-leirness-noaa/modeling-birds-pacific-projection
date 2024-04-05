390caa4b210c52fc5e26fdd12e74c70ad6e8e326
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-05 13:07:16 -0500

    Fix {target} store names

--- tar_config.R
+++ tar_config.R
@@ -1,7 +1,7 @@
 # configure separate targets projects
 targets::tar_config_set(script = "targets_covariate_processing.R",
-                        store = "covariate_processing",
+                        store = "_targets_covariate_processing",
                         project = "covariate_processing")
 targets::tar_config_set(script = "targets_modeling.R",
-                        store = "modeling",
+                        store = "_targets_modeling",
                         project = "modeling")
