201d979e494c40bb26ddee675cc38fd7560c58e2
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-05 13:07:49 -0500

    Add simple model fit for testing

--- targets_modeling.R
+++ targets_modeling.R
@@ -1,52 +1,31 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
-# Load packages required to define the pipeline:
+# load targets package
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
 
-# Set target options:
+# set target options
 tar_option_set(
-  packages = c("tibble") # Packages that your targets need for their tasks.
-  # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  #
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
+  format = "qs",
+  controller = crew::crew_controller_local(workers = 8, seconds_idle = 30)
 )
 
-# Run the R scripts in the R/ folder with your custom functions:
+# source R scripts in the R/ folder
 tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
+
+simple_model_func <- function(sp, dayofyear_k = -1, mgcv_gamma = 1, basis = "tp", dat_fn) {
+  dat <- readRDS(dat_fn) |>
+    tibble::as_tibble() |>
+    dplyr::rename("count" = toupper(sp))
+  form <- count ~ platform +
+    s(julianday, bs = basis) +
+    s(dayofyear, bs = "cc", k = dayofyear_k) +
+    s(depth, bs = basis)
+  mgcv::gam(form, data = dat, family = mgcv::nb(), gamma = mgcv_gamma)
+}
 
 list(
+  tar_target(atlantic_data_file, command = "data/segmented_data_analysis_2023-02-15.rds", format = "file"),
+  tar_target(mods, command = tibble::tibble(sp = c("atpu", "blki", "coei", "noga", "rtlo"))),
+  tar_target(simple_model,
+             command = simple_model_func(sp = mods$sp, dat_fn = atlantic_data_file),
+             pattern = map(mods),
+             iteration = "list")
 )
