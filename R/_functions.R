f318b9f6f35d07fde3ffd66fd381717ed325d313
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-10 14:49:43 -0500

    Fix syntax error and add code to remove `time` value in raster file

--- R/_functions.R
+++ R/_functions.R
@@ -31,7 +31,7 @@ create_intervals_daily <- function(file, round_dt = FALSE) {
 create_covariate_output <- function(file, start, end, fname, label, round_dt = FALSE) {
   library(lubridate)
   x <- stars::read_ncdf(file)
-  tm <- time(r)
+  tm <- time(x)
   if (round_dt) {
     tm <- (tm - 1) |>
       lubridate::round_date(unit = "day")
@@ -40,9 +40,13 @@ create_covariate_output <- function(file, start, end, fname, label, round_dt = F
     which()
   r <- x[, , , idx] |>  # dplyr::slice(time, idx) can be used if x is not a stars proxy object
     stars::st_as_stars() |>
-    terra::rast() |>
-    terra::mean()
+    terra::rast()
+  if (terra::nlyr(r) > 1) {
+    r <- terra::mean(r)
+  }
+  # terra::crs(r) <- "epsg:4326"
   names(r) <- label
+  terra::time(r) <- NULL
   terra::writeRaster(r, filename = file.path("output", paste0(fname, "-", label, ".tif")), overwrite = TRUE)
   file.path("output", paste0(fname, "-", label, ".tif"))
 }
--- scripts/_tar_make.R
+++ scripts/_tar_make.R
@@ -4,9 +4,7 @@ Sys.setenv(TAR_PROJECT = "covariate_processing")
 ptm <- Sys.time()
 targets::tar_make()
 Sys.time() - ptm
-# Time difference of 3.870845 mins
-# Time difference of 5.045395 mins for reading in netcdf file each time
-# Time difference of 2.936815 mins for reading in netcdf file each time, but with 10 workers
+# Time difference of
 
 man <- targets::tar_manifest()
 man
