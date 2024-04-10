1963b1770f6e459c1a92163b51e8b78221d865f4
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-10 14:48:50 -0500

    Update code to create daily files instead of monthly

--- targets_covariate_processing.R
+++ targets_covariate_processing.R
@@ -4,7 +4,7 @@ library(targets)
 # set target options
 tar_option_set(
   format = "qs",
-  controller = crew::crew_controller_local(workers = 10, seconds_idle = 30)
+  controller = crew::crew_controller_local(workers = parallel::detectCores() - 1, seconds_idle = 5)
 )
 
 # source necessary R scripts
@@ -34,32 +34,17 @@ targets <- tarchetypes::tar_map(
   values = values,
   names = output_fname,
   tar_target(file, command = data_source, format = "file", deployment = "main"),
-  tar_target(int_monthly, command = head(create_intervals_monthly(file)), deployment = "main"),
+  tar_target(int_daily, command = create_intervals_daily(file, round_dt = TRUE)),
   tar_target(covariates,
              command = create_covariate_output(file,
-                                               start = int_monthly$start,
-                                               end = int_monthly$end,
-                                               fname = paste0(output_fname, "-monthly"),
-                                               label = int_monthly$label),
-             pattern = map(int_monthly),
+                                               start = int_daily$start,
+                                               end = int_daily$end,
+                                               fname = paste0(output_fname, "-daily"),
+                                               label = int_daily$label,
+                                               round_dt = TRUE),
+             pattern = map(int_daily),
              format = "file",
              iteration = "vector"
   )
 )
 list(targets)
-
-# list(
-#   # wcra31_sst_daily
-#   tar_target(wcra31_sst_daily_file, command = "data/wcra31_sst_daily_1980_2010.nc", format = "file", deployment = "main"),
-#   tar_target(wcra31_sst_int_monthly, command = head(create_intervals_monthly(wcra31_sst_daily_file)), deployment = "main"),
-#   tar_target(covariates_wcra31_sst,
-#              command = create_covariate_output(wcra31_sst_daily_file,
-#                                                start = wcra31_sst_int_monthly$start,
-#                                                end = wcra31_sst_int_monthly$end,
-#                                                fname = "wcra31-sst-monthly",
-#                                                label = wcra31_sst_int_monthly$label),
-#              pattern = map(wcra31_sst_int_monthly),
-#              format = "file",
-#              iteration = "vector"
-#   )
-# )
