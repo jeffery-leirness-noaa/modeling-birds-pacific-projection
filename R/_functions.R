dee67a9740b8c50d635344c08391ee98e0bb4028
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-16 13:36:08 -0500

    Change function name

--- R/_functions.R
+++ R/_functions.R
@@ -27,7 +27,7 @@ create_intervals_daily <- function(file, round_dt = FALSE) {
                  label = start)
 }
 
-create_covariate_output <- function(file, start, end, fname, label, round_dt = FALSE) {
+process_covariate_file <- function(file, start, end, label, round_dt = FALSE) {
   library(lubridate)
   x <- stars::read_ncdf(file)
   tm <- time(x)
@@ -38,14 +38,33 @@ create_covariate_output <- function(file, start, end, fname, label, round_dt = F
   idx <- lubridate::date(tm) %within% lubridate::interval(start, end) |>
     which()
   r <- x[, , , idx] |>  # dplyr::slice(time, idx) can be used if x is not a stars proxy object
-    stars::st_as_stars() |>
+    stars::st_as_stars()
+
+  # fix lat/lon values that were incorrectly stored in netcdf file --------
+  nc <- ncdf4::nc_open(file)
+  lon <- ncdf4::ncvar_get(nc, "lon_rho")
+  lat <- ncdf4::ncvar_get(nc, "lat_rho")
+  ncdf4::nc_close(nc)
+  r <- stars::st_as_stars(list(sst = r[[1]]),
+                          dimensions = stars::st_dimensions(x = round(lon[, 1], 2),
+                                                            y = round(lat[1, ], 2),
+                                                            time = tm[idx])) |>
     terra::rast()
+  # -----------------------------------------------------------------------
+
   if (terra::nlyr(r) > 1) {
     r <- terra::mean(r)
+  } else {
+    terra::time(r) <- NULL
   }
   terra::crs(r) <- "epsg:4326"
   names(r) <- label
-  terra::time(r) <- NULL
+  terra::wrap(r)
+}
+
+create_covariate_output <- function(file, start, end, fname, label, round_dt = FALSE) {
+  r <- process_covariate_file(file, start = start, end = end, label = label, round_dt = round_dt) |>
+    terra::unwrap()
   terra::writeRaster(r, filename = file.path("output", paste0(fname, "-", label, ".tif")), overwrite = TRUE)
   file.path("output", paste0(fname, "-", label, ".tif"))
 }
@@ -59,7 +78,7 @@ process_covariate_data <- function(file, fname) {
 prepare_data <- function(path) {
   data.table::fread(path) |>
     tibble::as_tibble(.name_repair = janitor::make_clean_names) |>
-    dplyr::mutate(date = lubridate::as_date(paste(year, month, day, sep = "-")),
-                  lon = lon + 180) |>
+    dplyr::select(!(chla:index_pdo_lag12)) |>
+    dplyr::mutate(date = lubridate::as_date(paste(year, month, day, sep = "-"))) |>
     sf::st_as_sf(coords = c("lon", "lat"), crs = "WGS84")
 }
