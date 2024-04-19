40824753ab0c22e0214825595c4f56e8b90c76bd
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-19 18:16:13 +0000

    Add code to save output to specific storage container

--- scripts/submit-job-test.py
+++ scripts/submit-job-test.py
@@ -1,8 +1,11 @@
 # set testing parameters
 code = "./scripts"
-rscript_command = "Rscript test.R"
-dir_out = None
-dir_env = "./env"
+# rscript_command = "Rscript test.R"
+rscript_command = "Rscript test.R --dir_out=${{outputs.dir_out}}"
+# dir_out = None
+dir_out = "azureml://datastores/workspaceblobstore/paths/"
+# dir_env = "./env"
+environment = "azureml://registries/nccos-registry-ml/environments/nccos-leirness-modeling-birds-pacific-projection/versions/2"
 compute = "nccos-vm-leirness-e4dsv4"
 experiment_name = "r-data-access-testing"
 display_name = "r-data-access-testing-exp-1"
@@ -21,7 +24,10 @@ workspace = "nccos-mse-biogeo-seabird-ml"
 
 # get a handle to the workspace
 ml_client = MLClient(
-  DefaultAzureCredential(), subscription_id, resource_group, workspace
+  credential = DefaultAzureCredential(), 
+  subscription_id = subscription_id, 
+  resource_group_name = resource_group, 
+  workspace_name = workspace
 )
 
 # configure the command
@@ -29,11 +35,11 @@ if dir_out is None:
   outputs = None
 else:
   outputs = {"dir_out": Output(type = "uri_folder", path = dir_out, mode = "rw_mount")}
-if dir_env is None:
-  environment = None
-else:
-  # environment = Environment(build = BuildContext(path = dir_env))
-  environment = "azureml://registries/nccos-registry-ml/environments/nccos-leirness-modeling-birds-pacific-projection/versions/1"
+# if dir_env is None:
+#   environment = None
+# else:
+#   # environment = Environment(build = BuildContext(path = dir_env))
+#   environment = "azureml://registries/nccos-registry-ml/environments/nccos-leirness-modeling-birds-pacific-projection/versions/1"
 job = command(
   code = code,
   command = rscript_command,
