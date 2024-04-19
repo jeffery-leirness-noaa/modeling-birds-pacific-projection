548e98635feb693af9d18fe6bfd2d5e147844935
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-19 16:21:15 +0000

    Add code to parse options in Rscript job submission

--- scripts/test.R
+++ scripts/test.R
@@ -1,3 +1,16 @@
+# specify desired options in a list
+option_list <- list(
+  optparse::make_option("--dir_out",
+                        type = "character",
+                        dest = "dir_out",
+                        default = "output",
+                        help = "Output directory to use [default %default]")
+)
+
+# get command line options
+opt <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
+dir_out <- opt$dir_out
+
 # load mtcars dataset
 data(mtcars)
 
@@ -5,4 +18,4 @@ data(mtcars)
 print(mtcars)
 
 # save as RDS file
-# saveRDS(mtcars, file.path(dir_out, "mtcars.rds"))
+saveRDS(mtcars, file.path(dir_out, "mtcars.rds"))
