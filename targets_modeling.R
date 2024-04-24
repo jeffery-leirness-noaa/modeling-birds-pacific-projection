2d8d1b363740c566019e505733e074be9f72a53b
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-24 11:43:19 -0500

    Incorporate use of {config} package

--- targets_modeling.R
+++ targets_modeling.R
@@ -1,32 +1,35 @@
+# get configuration values
+config <- config::get(file = "config.yaml")
+
 # load targets package
 library(targets)
 
 # set target options
 tar_option_set(
   format = "qs",
-  controller = crew::crew_controller_local(workers = 8, seconds_idle = 30)
+  controller = crew::crew_controller_local(workers = parallel::detectCores() - 1, seconds_idle = 10)
 )
 
 # source R scripts in the R/ folder
 tar_source()
 
-simple_model_func <- function(sp, dayofyear_k = -1, mgcv_gamma = 1, basis = "tp", dat_fn) {
-  dat <- readRDS(dat_fn) |>
-    tibble::as_tibble() |>
-    dplyr::rename("count" = toupper(sp))
+simple_model_func <- function(.data, sp, dayofyear_k = -1, mgcv_gamma = 1, basis = "tp") {
   form <- count ~ platform +
     s(julianday, bs = basis) +
     s(dayofyear, bs = "cc", k = dayofyear_k) +
     s(depth, bs = basis)
-  mgcv::gam(form, data = dat, family = mgcv::nb(), gamma = mgcv_gamma)
+  mgcv::gam(form,
+            data = dplyr::rename(.data, "count" = toupper(sp)),
+            family = mgcv::nb(),
+            gamma = mgcv_gamma)
 }
 
 list(
-  tar_target(data_path, command = "data/segmented-data.csv", format = "file"),
-  tar_target(dat, command = prepare_data(data_path)),
+  tar_target(data_path, command = config$dataset, format = "file"),
+  tar_target(data, command = prepare_data(data_path)),
   tar_target(mods, command = tibble::tibble(sp = c("atpu", "blki", "coei", "noga", "rtlo"))),
   tar_target(simple_model,
-             command = simple_model_func(sp = mods$sp, dat_fn = atlantic_data_file),
+             command = simple_model_func(.data = data, sp = mods$sp),
              pattern = map(mods),
              iteration = "list")
 )
