86e14a308d2a577ef50fa41af53cc1d87ca2893c
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-04-30 13:40:43 -0500

    Add comments

--- tar_make.R
+++ tar_make.R
@@ -4,6 +4,7 @@ Sys.setenv(TAR_PROJECT = "covariate_processing")
 ptm <- Sys.time()
 targets::tar_make()
 Sys.time() - ptm
+# Time difference of 15.40642 mins
 
 targets::tar_manifest()
 targets::tar_visnetwork(targets_only = TRUE)
