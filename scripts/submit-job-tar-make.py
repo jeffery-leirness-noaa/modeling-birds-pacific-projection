1dcaa6a43a4a1320f4256a50c6b3d3b9e37963ff
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-06-13 19:26:17 +0000

    Update scripts to use bird data in RDS format

--- scripts/submit-job-tar-make.py
+++ scripts/submit-job-tar-make.py
@@ -1,10 +1,8 @@
 # set parameters
 code = "."
 rscript_command = "Rscript tar_make.R --dir_in=${{inputs.dir_in}} --dir_out=${{outputs.dir_out}}"
-# dir_in = "azureml://datastores/datastor_raw/paths/"
-dir_in = "https://nccospacificsbdatastor.blob.core.windows.net/raw"
-# dir_out = "azureml://datastores/datastor_processing/paths/"
-dir_out = "https://nccospacificsbdatastor.blob.core.windows.net/processing"
+dir_in = "azureml://datastores/datastor_raw/paths/"
+dir_out = "azureml://datastores/datastor_processing/paths/"
 environment = "azureml://registries/nccos-registry-ml/environments/nccos-leirness-modeling-birds-pacific-projection/versions/4"
 compute = "nccos-vm-leirness-e4dsv4"
 experiment_name = "targets-covariate-processing"
