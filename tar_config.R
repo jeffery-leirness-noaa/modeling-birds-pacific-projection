2d73868cb184e7ecf72a2259297dce976a1b1f97
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-08-26 15:36:50 +0000

    Remove files specific to {targets} package

--- tar_config.R
+++
@@ -1,7 +0,0 @@
-# configure separate targets projects
-targets::tar_config_set(script = "targets_covariate_processing.R",
-                        store = "_targets_covariate_processing",
-                        project = "covariate_processing")
-targets::tar_config_set(script = "targets_modeling.R",
-                        store = "_targets_modeling",
-                        project = "modeling")
