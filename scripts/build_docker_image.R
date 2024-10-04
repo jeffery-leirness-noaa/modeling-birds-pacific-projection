1afccd5cba23cdfffa4efbf6b70d32f86d4b9509
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-10-04 17:21:13 +0000

    Update docker image build process

--- scripts/build_docker_image.R
+++ scripts/build_docker_image.R
@@ -2,18 +2,34 @@ tag <- lubridate::now() |>
   lubridate::as_date() |>
   stringr::str_replace_all(pattern = "-", replacement = ".")
 
-stringr::str_c("docker build github.com/jeffery-leirness-noaa/modeling-birds-pacific-projection:", tag)
+# stringr::str_c("docker build github.com/jeffery-leirness-noaa/modeling-birds-pacific-projection:", tag)
+#
+# build_docker_image <- function(repo, tag = "latest") {
+#   if (tag == "date") {
+#     tag <- lubridate::now() |>
+#       lubridate::as_date() |>
+#       stringr::str_replace_all(pattern = "-", replacement = ".")
+#   }
+#   stringr::str_c("docker build -t ", repo, ":", tag, " .")
+# }
+#
+# build_docker_image(repo = "jefferyleirnessnoaa/modeling-birds-pacific-projection",
+#                    tag = "date") |>
+#   system2()
+# # system2("docker push jefferyleirnessnoaa/modeling-birds-pacific-projection")
 
-build_docker_image <- function(repo, tag = "latest") {
-  if (tag == "date") {
-    tag <- lubridate::now() |>
-      lubridate::as_date() |>
-      stringr::str_replace_all(pattern = "-", replacement = ".")
-  }
-  stringr::str_c("docker build -t ", repo, ":", tag, " .")
-}
+# pracpac::use_docker()
+# pracpac::create_docker_dir()
+# pracpac::renv_deps()
 
-build_docker_image(repo = "jefferyleirnessnoaa/modeling-birds-pacific-projection",
-                   tag = "date") |>
-  system2()
-# system2("docker push jefferyleirnessnoaa/modeling-birds-pacific-projection")
+docker_dir <- "docker"
+fs::file_copy(renv::paths$lockfile(),
+              new_path = fs::path(docker_dir, "renv.lock"),
+              overwrite = TRUE)
+image_tag <- tag
+image_user <- "jeffery-leirness-noaa"
+image_repo <- "modeling-birds-pacific-projection"
+buildcmd <- glue::glue("docker build --tag ghcr.io/{image_user}/{image_repo}:{image_tag} {docker_dir}")
+# system(buildcmd)
+pushcmd <- glue::glue("docker push ghcr.io/{image_user}/{image_repo}:{image_tag}")
+# system(pushcmd)
