4a7dd8cbd5adc4e7f5042c33e313bbf81b2fb208
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-10-28 17:26:36 +0000

    Add support for GeoTiff files in create_targets_data_command() function

--- R/create_targets_data_command.R
+++ R/create_targets_data_command.R
@@ -1,9 +1,15 @@
-# this will fail for .tiff files when local = TRUE!
 create_targets_data_command <- function(file_name, local = TRUE) {
   if (local) {
-    fs::path(opt$dir_in, !!file_name) |>
-      readr::read_csv() |>
-      rlang::expr()
+    ext <- fs::path_ext(file_name)
+    if (ext == "csv") {
+      fs::path(opt$dir_in, !!file_name) |>
+        readr::read_csv() |>
+        rlang::expr()
+    } else if (ext %in% c("tif", "tiff")) {
+      fs::path(opt$dir_in, !!file_name) |>
+        terra::rast() |>
+        rlang::expr()
+    }
   } else {
     token <- azure_auth_token()
     AzureStor::storage_endpoint(Sys.getenv("TARGETS_ENDPOINT"), token = !!token) |>
