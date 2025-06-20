86a6463f1ebe5f32f3c3a0bd8c259bc1f01f7ff3
Jeffery Leirness <jeffery.leirness@noaa.gov>
2025-06-20 14:00:50 +0000

    Add additional comments

--- R/make_predictions.R
+++ R/make_predictions.R
@@ -15,12 +15,18 @@
 #' @examples
 #' # Make predictions for 2010 using a fitted model
 #' predictions <- make_predictions(
-#'   model_fit, 
+#'   model_fit,
 #'   data = data_prediction_gfdl_2010,
 #'   label = "reanalysis",
 #'   add = list(depth = data_bathy_10km, slope = data_slope_10km),
 #'   mask = study_polygon
 #' )
+# TO DO: change function default to `mask = NULL`
+# TO DO: add option for object to be a workflow or parsnip object. If parsnip
+# object, must also supply pre-processing recipe that will need to be applied
+# (i.e., `recipes::bake()`) prior to calling `predict()`. Doing this could
+# drastically reduce storage costs because fitted parsnip models could be
+# "butchered" using `butcher::butcher()` to reduce object size.
 make_predictions <- function(object, data, label, add, mask) {
   new_data <- prepare_data_prediction(data, label = label, add = add,
                                       mask = mask)
